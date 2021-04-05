343db152c340c78bbc538d8cdd382cf5a2185c45
Mathieu Voland <mathieu.voland@gmail.com>
2021-04-05 12:39:05 +0200

    Log API baseUrl

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -8,6 +8,9 @@ if (process.env.NODE_ENV === 'production') {
     baseUrl = "http://localhost:8080/api/";
 }
 
+console.log("Base URL");
+console.log(baseUrl)
+
 export default axios.create({
     baseURL: baseUrl,
     responseType: "json"
