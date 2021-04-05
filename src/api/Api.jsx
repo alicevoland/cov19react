0ebc11f9b210c6d914f5254b6f8c92b98c2d4c18
Mathieu Voland <mathieu.voland@gmail.com>
2021-04-05 12:02:35 +0200

    Change API url for production

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,6 +1,14 @@
 import axios from "axios";
 
+
+var baseUrl;
+if (process.env.NODE_ENV === 'production') {
+    baseUrl = "https://cov19api.herokuapp.com/api/";
+} else {
+    baseUrl = "http://localhost:8080/api/";
+}
+
 export default axios.create({
-    baseURL: "http://localhost:8080/api/",
+    baseURL: baseUrl,
     responseType: "json"
 });
